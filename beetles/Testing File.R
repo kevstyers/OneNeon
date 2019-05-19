c5f378b76f186f019de10e53ba0b72cce0b00b32
Kevin <45089457+kevstyers@users.noreply.github.com>
2019-05-18 22:53:13 -0600

    small changes

--- beetles/Testing File.R
+++ beetles/Testing File.R
@@ -7,6 +7,7 @@ library(data.table)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(stringr)
 library(plotly)
 
 # Grab all the data (takes like 10 minutes)
@@ -33,20 +34,23 @@ sort <- sort[!(is.na(sort$bet_sorting.taxonID) | sort$bet_sorting.taxonID == "")
 
 # Initial filter for the test plot
 sort2018 <- sort %>%
-  filter(bet_sorting.setDate > "2018-01-01" & bet_sorting.setDate < "2019-01-01") %>%
+  filter(bet_sorting.setDate > "2014-01-01" & bet_sorting.setDate < "2018-01-01") %>%
   #filter(bet_sorting.sampleType %in% carabids) %>%
   select(bet_sorting.domainID,bet_sorting.siteID,bet_sorting.collectDate,bet_sorting.plotID,bet_sorting.trapID,
          bet_sorting.setDate,bet_sorting.sampleType,bet_sorting.taxonID,bet_sorting.scientificName,
          bet_sorting.taxonRank,bet_sorting.individualCount,bet_sorting.identificationQualifier,bet_sorting.nativeStatusCode,
          bet_sorting.identifiedBy) %>%
   mutate(year = substring(bet_sorting.setDate,1,nchar(bet_sorting.setDate) -1)) %>%
-  mutate(monthDay = substring(bet_sorting.setDate, 6))
+  mutate(monthDay = substring(bet_sorting.setDate, 6)) %>%
+  mutate(genus = word(bet_sorting.scientificName,1))
+  Dataframe1$COL2 <- word(Dataframe2$COL1,1)
 
 
 
 sort2018$monthDay <- as.Date(sort2018$monthDay, format = "%m-%d")
 ## Factorize that data frame
 sort2018 <- as.data.frame(unclass(sort2018))
+sort <- as.data.frame(unclass(sort))
 ## Sort data by idenified by to make the bar plot pretty
 sortFactor <- within(sortFactor, 
                      bet_sorting.identifiedBy <- factor(bet_sorting.identifiedBy, 
@@ -120,21 +124,21 @@ sort2018f <- sort2018 %>%
   filter(bet_sorting.siteID %in% sites)
 
 sort2018f <- within(sort2018f, 
-                    bet_sorting.scientificName <- factor(bet_sorting.scientificName, 
-                                                  levels=names(sort2018f(table(bet_sorting.scientificName), 
+                    genus <- factor(genus, 
+                                                  levels=names(sort2018f(table(genus), 
                                                                      decreasing=FALSE))))
 
-plot<- ggplot(sort2018f, aes(x= bet_sorting.setDate,fill = bet_sorting.identifiedBy))+
-  geom_density(alpha = .2) +
-  labs(title = "Total Beetles Counts by Plot ID for D10.13 in 2018",
+plot<- ggplot(sort2018, aes(x= monthDay,fill = genus))+
+  geom_density(alpha = .35) +
+  labs(title = "Total Beetles Counts by Site ID for D10.13 in 2017",
        x = "Domain",
        y = "Count") +
-  theme(axis.text.x = element_text(angle = 270)) 
-#facet_wrap(~year)
+  theme(axis.text.x = element_text(angle = 270)) +
+  facet_wrap(~bet_sorting.siteID)
 
 start <- Sys.time()
 ggplotly(plot)
 end <- Sys.time()
 end - start
-
+  
 
